8fc1fb17b3a1ff7334613ef2a412da70267eb8c2
Rrobinvip <rrobinvip@live.cn>
2023-03-12 13:21:09 -0400

    - macOS auto web finished.

---
+++ macOS_auto.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+
+print(" * Automated script started..")
+# get the path to your Python script relative to the current working directory
+script_path = os.path.join("macOS_scripts", "browse_web.py")
+
+print(" - Process 1 starting..")
+# execute the script as a child process
+process = subprocess.Popen(["python", script_path])
+
+# wait for the child process to finish
+process.wait()
+
+# the parent process will resume here once the child process has finished
+print(" - Process 1 has finished.")
